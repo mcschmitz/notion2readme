218ab64cfa3287d018e077e9b773534b42998ca3
Manuel Schmitz <manuel@e-bot7.com>
2020-12-08 17:15:32 +0100

    Add Readme and correct Headings

---
+++ ReadMe.md
@@ -0,0 +1,48 @@
+# Notion2ReadMe
+
+As an ironic matter of fact this readme is written manually and does not 
+come from a Notion page. 
+
+Notion2ReadMe can be used as a Github Action to convert a Notion Page to a 
+Github-readable ReadMe.md file containing subpages, code, links etc.
+
+To use the Action just add the following YAML-File in the `.
+github/workflows` directory of you repository.
+
+```yaml
+name: Notion2Readme
+
+on:
+  push:
+    branches:
+      - '*'
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: actions/checkout@v2
+      - name: Notion2Readme
+        uses: mcschmitz/notion2readme@main
+        with:
+          url: '< URL to Notion Page >'
+        env:
+          TOKEN_V2: ${{ secrets.token_v2 }}
+      - name: Push Readme
+        run: |
+          git config user.name github-actions
+          git config user.email github-actions@github.com
+          git add .
+          git commit -m "NOTION2README"
+          git push
+```
+
+
+This will execute the synchronization with Notion on every push to any branch.
+To change that you can need to define specific branches. 
+To run the action you will have to att the `token_v2` secret to your github 
+repository. The `token_v2` token can be obtained in any browser running an 
+active Notion session. Please make sure, that the user linked to the token 
+has access to the Notion Page you want to convert.
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import argparse
-import notion2md
-import os
 import shutil
+import os
+from notion.client import NotionClient
+from notion2md.exporter import PageBlockExporter
 
 parser = argparse.ArgumentParser()
 
@@ -10,6 +11,26 @@ parser.add_argument("--token_v2", type=str)
 parser.add_argument("--url", type=str)
 
 args = parser.parse_args()
+
+OUTPUT_FOLDER = './README/'
+
+
+class GitHubPageBlockExporter(PageBlockExporter):
+    def __init__(self, url, client, blog_mode):
+        super().__init__(url=url,client=client, blog_mode=blog_mode)
+        if not self.bmode:
+            self.file_name = self.page.title
+            self.md = f"# {self.page.title} \n\n"
+
+
+def export_notion(token_v2="", url=""):
+    if not (os.path.isdir(OUTPUT_FOLDER)):
+        os.makedirs(os.path.join(OUTPUT_FOLDER))
+    client = NotionClient(token_v2=token_v2)
+    exporter = GitHubPageBlockExporter(url, client, blog_mode=False)
+    exporter.create_main_folder(OUTPUT_FOLDER)
+    exporter.create_file()
+    export(exporter)
 
 
 def find_markdown(directory: str):
@@ -21,8 +42,20 @@ def find_markdown(directory: str):
                 return file
 
 
+def export(exporter):
+    """Recursively export page block with its sub pages
+
+        Args:
+            exporter(PageBlockExporter()): export page block
+    """
+    exporter.page2md()
+    exporter.write_file()
+    for sub_exporter in exporter.sub_exporters:
+        export(sub_exporter)
+
+
 if __name__ == '__main__':
-    notion2md.export_cli(args.token_v2, args.url, bmode=0)
-    md_path = find_markdown(directory="notion2md_output")
-    shutil.move(md_path, "README.md")
-    shutil.rmtree("notion2md_output")
+    export_notion(args.token_v2, args.url)
+
+    md_path = find_markdown(directory=OUTPUT_FOLDER)
+    shutil.move(md_path, "ReadMe.md")
